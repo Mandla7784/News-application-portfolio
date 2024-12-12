6997be8f73ed3eb79d61652fe60ee175ace4c798
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-12 14:36:50 +0200

    Added then to habdle promise instead of async await

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const url_base_path =
-  "https://newsapi.org/v2/everything?q=Apple&from=2024-12-12&sortBy=popularity&apiKey=API_KEY";
+  "https://newsapi.org/v2/top-headlines?country=us&apiKey=8f0e4148f524405c9aebb149ee25bed2";
+
 /**
  *
  * @param {*} path
  * this function makes an http request for news data
  * @returns {object} data
  */
-async function getAllNews(path) {
-  const response = await fetch(path);
-  const data = await response.json();
-  console.log(data);
+async function getAllNewsArticles(path) {
+  fetch(path)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const { articles } = data;
+      console.log(articles);
+    })
+    .catch((erro) => console.log(erro));
 }
 
 getAllNews(url_base_path);
