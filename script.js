57a6b0cb564afb1c314f6273d458986e40f059b5
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-12 14:16:38 +0200

    Updates

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const url_base_path =
   "https://newsapi.org/v2/everything?q=Apple&from=2024-12-12&sortBy=popularity&apiKey=API_KEY";
+/**
+ *
+ * @param {*} path
+ * this function makes an http request for news data
+ * @returns {object} data
+ */
 async function getAllNews(path) {
   const response = await fetch(path);
+  const data = await response.json();
+  console.log(data);
 }
+
+getAllNews(url_base_path);
