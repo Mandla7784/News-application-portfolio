6db8cec4c041ee283923c82ef5d0ce2e68524fa1
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-12 14:15:25 +0200

    Added a functionto get news

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Connedtd");
+const url_base_path =
+  "https://newsapi.org/v2/everything?q=Apple&from=2024-12-12&sortBy=popularity&apiKey=API_KEY";
+async function getAllNews(path) {
+  const response = await fetch(path);
+}
